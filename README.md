06b4521aac34485720e744a2a8982e3c55e50437
Jiro Lin <jiro.lin9611@gmail.com>
2015-09-01 23:25:58 +0800

    push the same project to the github

---
+++ README.md
@@ -0,0 +1,25 @@
+# jiro_sample_project
+
+A sample project that provide people quickly build up develop environment
+
+## Require
+
+npm
+
+gulp
+
+bower
+
+### Steps:
+
+npm install
+
+gulp build
+
+
+********
+
+Remember remove "node_modules" and "bower_components" before you push!!!!!!!!
+
+
+********
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+var gulp = require('gulp');
+var debug = require('gulp-debug');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+var gulpFilter = require('gulp-filter');
+var mainBowerFiles = require('main-bower-files');
+var runSequence = require('run-sequence');
+var install = require('gulp-install');
+var rimraf = require('rimraf');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+
+var js_dest_path = 'assets/lib/js';
+var css_dest_path = 'assets/lib/css';
+var img_for_fancybox = 'assets/lib/css';
+var font_path = 'assets/lib/fonts';
+
+var jsFilter = gulpFilter('*.js');
+var cssFilter = gulpFilter(['*.css']);
+var imgFilter = gulpFilter(['*.gif', '*.png']);
+var fontFilter = gulpFilter(['*.eot', '*.svg', '*.ttf', '*.woff*']);
+
+var files = [
+  './**/*.*',
+  '!./node_modules/**',
+  '!./bower_components/**'
+];
+
+//Compile sass/scss To CSS
+gulp.task('styles',function(){
+    gulp.src('battleslot/style.{sass,scss}')
+    .pipe(sass())
+    .pipe(gulp.dest('battleslot'));
+});
+
+gulp.task('build', function(callback) {
+    runSequence('clean', 'install', 'exportBowerFiles', callback);
+});
+
+gulp.task('install', function() {
+    return gulp.src(['./bower.json', './package.json'])
+        .pipe(install());
+});
+
+gulp.task('clean', function() { 
+    rimraf("assets/lib", function(){});
+    return rimraf("bower_components", function(){});
+
+});
+
+gulp.task('exportBowerFiles', function() {
+    return gulp.src(mainBowerFiles())
+
+    .pipe(jsFilter)
+        .pipe(gulp.dest(js_dest_path))
+        .pipe(jsFilter.restore())
+
+    .pipe(cssFilter)
+        .pipe(minifyCss())
+        .pipe(gulp.dest(css_dest_path))
+        .pipe(cssFilter.restore())
+
+    .pipe(imgFilter)
+        .pipe(gulp.dest(img_for_fancybox))
+        .pipe(imgFilter.restore())
+
+    .pipe(fontFilter)
+        .pipe(gulp.dest(font_path))
+});
+
+gulp.task('watch', function () {
+  return gulp.watch(files).on('change', browserSync.reload);
+});
+
+gulp.task('browser', function () {
+  browserSync.init(files, {
+    startPath: '/',
+    server: {
+      baseDir: './'
+    },
+    browser: 'default'
+  });
+});
+
+gulp.task('serve', function () {
+  runSequence('build', 'watch', 'browser');
+});
